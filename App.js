7e1cc37a76a07822cd0d7c2230805cf210a876c2
mzecic <matejzecic@gmail.com>
2022-12-13 08:58:27 -0500

    add controlled input logic

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState(null);
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+
+  if(userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
-    <LinearGradient colors={[ "#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        imageStyle={styles.backgroundImage}
+        style={styles.rootScreen}
+        resizeMode="cover"
+        source={require("./assets/images/dices_unsplash.jpg")}
+      >
+      <SafeAreaView style={styles.rootScreen}>
+        {screen}
+      </SafeAreaView>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,22 +1,49 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
-export default function StartGamePage() {
+export default function StartGameScreen({ onPickNumber }) {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
+
+    onPickNumber(enteredNumber);
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         autoCorrect="false"
-        autoCapitalize="none"
         keyboardType="number-pad"
         maxLength={2}
         style={styles.numberInput}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
